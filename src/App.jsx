f7a9d8459b8b0832ba19eaf72f530e203eeffca1
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-05 17:12:11 +0100

    popup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles/clocking.css'
 import Clocking from './Pages/clocking'
-
+import Popup from './components/Popup'
 
 function App() {
 
@@ -13,6 +13,7 @@ function App() {
      <Routes>
       <Route index element = {<Clocking/>}/>
       <Route path='/clocking' element ={<Clocking/>}/>
+      <Route path='/popup' element ={<Popup/>}/>
      </Routes>
      </BrowserRouter>
     </>
