188abe6dd04dc9cd4ff74474c60f1b7d6dfe1954
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-05 17:37:27 +0100

    user details

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -3,14 +3,36 @@ import Profile from '../assets/images/profile.png'
 import close from '../assets/icons/close.png'
 
 const Popup = () => {
+
   return (
     <div className='popup'>
         <div className="exit">
+        <p className='clockInText'>Clock in</p>
             <img src={close} alt="" />
         </div>
         <div className="user">
             <div className="profilePic">
-                <img src={Profile} alt="" />
+                <img className='pic' src={Profile} alt="" />
+<p className='jobRole'>Frontend developer</p>
+            </div>
+
+            <div className="userDetails">
+                <div className="subject">
+                    <p className='title'> Name:</p>
+                    <p className='info'> Sarah Duke</p>
+                </div>
+                <div className="subject">
+                    <p className='title'>Card Id:</p>
+                    <p className='info'> 009867</p>
+                </div>
+                <div className="subject">
+                    <p className='title'> Email</p>
+                    <p className='info'> sarahduke@gmail.com</p>
+                </div>
+                <div className="subject">
+                    <p className='title'> Phone number</p>
+                    <p className='info'> 08026784895</p>
+                </div>
             </div>
         </div>
     </div>
