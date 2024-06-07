f74c4d2b3a03bd99a68e02477978c94616d51ea8
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-07 05:43:06 +0100

    timesheet

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,40 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import search from "../assets/icons/Component 248.png";
+import Filter from "../assets/icons/funnel.png";
 const Title = () => {
   return (
-    <div className='Title'>  
-       <div className='titleHead'> <h1>Timesheet</h1> </div>
+    <div className="Title">
+      <div className="titleHead">
+        <h1>Timesheet</h1>
+      </div>
 
-<div className="statusHead">
-    <div className="status">
-        <p className='clockIn'>Clocked in</p>
-        <p className='clockLate'>Clocked late</p>
-        <p className='offToday'>Off Today</p>
+      <div className="statusHead">
+        <div className="status">
+          <p className="clockIn">Clocked in</p>
+          <p className="clockLate">Clocked late</p>
+          <p className="offToday">Off Today</p>
         </div>
 
         <div className="right">
-           <button className='filterButton'>
-            <img className='filter' src={""} alt="" />
-            </button> 
-            <input 
-            type="searchbox" 
-            placeholder='Card ID, Role or Staff name'/>
+          <button className="filterButton">
+            <img className="filter" src={Filter} alt="" />
+          </button>
+          <input type="searchbox" placeholder="Card ID, Role or Staff name" />
+          <img className="search-icon" src={search} alt="" />
         </div>
-<hr />
-</div>
+        <hr />
+      </div>
 
-<table>
-    <tr>
-        <th>Staff Name</th>
-        <th>Card ID </th>
-        <th>Email Address </th>
-        <th>Role</th>
-        <th>Status </th>
-        <th>Date </th>
-    </tr>
-</table>
+      <table>
+        <tr>
+          <th>Staff Name</th>
+          <th>Card ID </th>
+          <th>Email Address </th>
+          <th>Role</th>
+          <th>Status </th>
+          <th>Date </th>
+        </tr>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default Title
+export default Title;
