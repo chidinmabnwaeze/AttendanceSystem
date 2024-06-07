1089c4a48a7b5cd4be65537ef9cef44f9dec4a64
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-07 05:20:57 +0100

    titleHead

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,14 +2,36 @@ import React from 'react'
 
 const Title = () => {
   return (
-    <div>  
-        <h1>Timesheet</h1>
+    <div className='Title'>  
+       <div className='titleHead'> <h1>Timesheet</h1> </div>
+
+<div className="statusHead">
+    <div className="status">
         <p className='clockIn'>Clocked in</p>
         <p className='clockLate'>Clocked late</p>
         <p className='offToday'>Off Today</p>
+        </div>
+
+        <div className="right">
+           <button className='filterButton'>
+            <img className='filter' src={""} alt="" />
+            </button> 
+            <input 
+            type="searchbox" 
+            placeholder='Card ID, Role or Staff name'/>
+        </div>
 <hr />
+</div>
+
 <table>
-    
+    <tr>
+        <th>Staff Name</th>
+        <th>Card ID </th>
+        <th>Email Address </th>
+        <th>Role</th>
+        <th>Status </th>
+        <th>Date </th>
+    </tr>
 </table>
     </div>
   )
