2712f5e305c7437ab69ce8a8108979bd2006af0f
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-08 16:55:42 +0100

    form

--- src/Pages/leave.jsx
+++ src/Pages/leave.jsx
@@ -10,15 +10,43 @@ const leave = () => {
       <Sidebar />
       <Title />
 
+<div className="form-border">
       <form action="">
         <div className="name">
           <label htmlFor="name">
-            {" "}
             Name of staff
             <input type="text" placeholder="Select staff" />
           </label>
         </div>
+        <div className="name">
+          <label htmlFor="name">
+            Staff Role
+            <input type="text" placeholder="Staff role" />
+          </label>
+        </div>
+        <div className="name">
+          <label htmlFor="name">
+           Purpose of leave
+            <input type="text" placeholder="Enter purpose of leave" />
+          </label>
+        </div> 
+        <div className="name">
+          <label htmlFor="name">
+          Description
+            <input type="textarea" placeholder="Describe your leave" textarea ="" />
+          </label>
+        </div>
+        <div className="name">
+          <label htmlFor="name">
+          Leave duration
+            <input type="textarea" placeholder="Describe your leave" textarea ="" />
+          </label>
+        </div>
       </form>
+    </div>
+
+    <button className="cancel">Cancel</button>
+    <button className="approve">Approve Leave</button>
     </div>
   );
 };
