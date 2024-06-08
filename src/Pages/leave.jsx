601da98936a7447a8027dc0abdcbec0c995d1360
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-08 11:19:14 +0100

    update

--- src/Pages/leave.jsx
+++ src/Pages/leave.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import Title from '../components/Title'
-import Header from '../components/Header'
-import Sidebar from '../components/Sidebar'
+import React from "react";
+import Title from "../components/Title";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const leave = () => {
   return (
     <div>
-        <Header/>
-        <Sidebar/>
-        <Title/>
+      <Header />
+      <Sidebar />
+      <Title />
 
-        <form action="">
-            <div className="name">
-            <label htmlFor="name"> Name of staff
-<input type="text" placeholder='Select staff'/>
-            </label>
-            </div>
-        </form>
+      <form action="">
+        <div className="name">
+          <label htmlFor="name">
+            {" "}
+            Name of staff
+            <input type="text" placeholder="Select staff" />
+          </label>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default leave
+export default leave;
