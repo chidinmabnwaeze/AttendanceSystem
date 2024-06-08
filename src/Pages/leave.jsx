6057037a81b61d100b3615277952b01866a3eca6
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-08 17:15:21 +0100

    leave

--- src/Pages/leave.jsx
+++ src/Pages/leave.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Title from "../components/Title";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import "../styles/leave.css"
 
 const leave = () => {
   return (
@@ -12,41 +13,50 @@ const leave = () => {
 
 <div className="form-border">
       <form action="">
+
         <div className="name">
-          <label htmlFor="name">
+          <label  className= "staff" htmlFor="name">
             Name of staff
             <input type="text" placeholder="Select staff" />
           </label>
         </div>
+
         <div className="name">
-          <label htmlFor="name">
+          <label className= "staff" htmlFor="name">
             Staff Role
             <input type="text" placeholder="Staff role" />
           </label>
         </div>
+
         <div className="name">
-          <label htmlFor="name">
+          <label className= "staff" htmlFor="name">
            Purpose of leave
             <input type="text" placeholder="Enter purpose of leave" />
           </label>
         </div> 
+
         <div className="name">
-          <label htmlFor="name">
+          <label className= "staff" htmlFor="name">
           Description
             <input type="textarea" placeholder="Describe your leave" textarea ="" />
           </label>
         </div>
+
         <div className="name">
-          <label htmlFor="name">
+          <label className= "staff" htmlFor="name">
           Leave duration
             <input type="textarea" placeholder="Describe your leave" textarea ="" />
           </label>
         </div>
-      </form>
-    </div>
 
+      </form>
+
+      <div className="button">
     <button className="cancel">Cancel</button>
     <button className="approve">Approve Leave</button>
+    </div>
+    </div>
+
     </div>
   );
 };
