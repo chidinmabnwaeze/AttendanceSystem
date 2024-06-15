eaa2e6a6086be4dbe2ef795f7384d7b0c8e6663c
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-15 07:28:03 +0100

    changes

---
+++ src/components/Tablist.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Tablist = () => {
+  return (
+    <div className='Tablist'>
+       <nav className='tab-nav'>
+        <ul className='tab-menu'>
+            <li className='tab-item'>
+                <button className='tab-button'>Clock-in</button>
+            </li>
+
+        </ul>
+        </nav> 
+    </div>
+  )
+}
+
+export default Tablist
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,16 +3,14 @@ import search from "../assets/icons/Component 248.png";
 import Filter from "../assets/icons/funnel.png";
 import Table from "./Table";
 
+const Title = ({ text, sub, sub1, sub2 }) => {
+  const [toggle, setToggle] = useState(1);
+  console.log(toggle);
 
-const Title = ({text, sub ,sub1 ,sub2}) => {
-
-const [toggle, setToggle] = useState(1)
-console.log(toggle)
-
- const updateToggle = (index)=>{
-// setToggle(()=>{index})
-setToggle(index)
-}
+  const updateToggle = (index) => {
+    // setToggle(()=>{index})
+    setToggle(index);
+  };
 
   return (
     <div className="Title">
@@ -22,26 +20,53 @@ setToggle(index)
 
       <div className="statusHead">
         <div className="status">
-        <li className={toggle === 1 ? " tab active-tab" : "tab"} onClick={()=> updateToggle(1)}>{sub}</li>
-          <li className={toggle === 2 ? " tab active-tab" : "tab"} onClick={()=> updateToggle(2)}>{sub1}</li>
-          <li className={toggle === 3 ? " tab active-tab" : "tab"} onClick={()=> updateToggle(3)}>{sub2}</li>
+          <li
+            className={toggle === 1 ? " tab active-tab" : "tab"}
+            onClick={() => updateToggle(1)}
+          >
+            {sub}
+          </li>
+          <li
+            className={toggle === 2 ? " tab active-tab" : "tab"}
+            onClick={() => updateToggle(2)}
+          >
+            {sub1}
+          </li>
+          <li
+            className={toggle === 3 ? " tab active-tab" : "tab"}
+            onClick={() => updateToggle(3)}
+          >
+            {sub2}
+          </li>
         </div>
 
         <div className="right">
           <button className="filterButton">
             <img className="filter" src={Filter} alt="" />
           </button>
-          <input className= 'input' type="searchbox" id="input" placeholder="Card ID, Role or Staff name" />
+          <input
+            className="input"
+            type="searchbox"
+            id="input"
+            placeholder="Card ID, Role or Staff name"
+          />
           <img className="search-icon" id="input" src={search} alt="" />
         </div>
-       
       </div>
       <hr />
 
-
-      <div className="content">
-<Table/>
+      {/* <div className={toggle === 2 ? "content active-tab" : "content"}>
+        <Table />
       </div>
+
+      <div className={toggle === 3 ? "content active-tab" : "content"}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus
+          incidunt id, nostrum deserunt illo placeat vero ea aperiam, enim velit
+          veniam perferendis quaerat tempora. Adipisci aliquam aut maxime quas
+          iste.
+        </p>
+      </div> */}
     </div>
   );
 };
