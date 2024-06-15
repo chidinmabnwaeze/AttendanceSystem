b0a1e7f13bb30d8489c1c4f20f83152c00a7064e
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-06-15 14:54:56 +0100

    test

--- src/components/Tablist.jsx
+++ src/components/Tablist.jsx
@@ -5,43 +5,51 @@ const Tablist = () => {
   const tabData = [
     {
       button: "Clocked In",
-      content : "this is tab 1 content"
+      content: "this is tab 1 content",
     },
     {
       button: "Clocked Out",
-      content : "this is tab 2 content"
+      content: "this is tab 2 content",
     },
     {
       button: "Off Today",
-      content : <Table/>
+      content: <Table />,
     },
   ];
 
   const [tabs, setTabs] = useState(0);
-//   const handleTabClick = (index) => {
-//     setTabs(index);
-//   };
+  //   const handleTabClick = (index) => {
+  //     setTabs(index);
+  //   };
 
   return (
     <div className="Tablist" role="tabpanel">
-      {tabData.map((data, index) => (
-        <nav className="tab-nav" key={index}>
-          <ul className="tab-menu">
-            <li className={`${index === tabs && "active"} tab-item`}
-            onClick={() =>setTabs(index)} >
-            {/* //  id="1" aria-labelledby="label" */}
-              {data.button}
-            </li>
-          </ul>
-        </nav>
-      ))}
-
+      <div className="tab-header">
+        {tabData.map((data, index) => (
+          <nav className="tab-nav" key={index}>
+            <ul className="tab-menu">
+                <div className="activeTabIndicator">
+              <li
+                className={`${index === tabs && "active"} tab-item ${<hr/>}`}
+                onClick={() => setTabs(index)}
+              >
+                {/* //  id="1" aria-labelledby="label" */}
+                {data.button}
+                {/* <hr /> */}
+              </li>
+              {/* <span className=""><hr /></span> */}
+              </div>
+            </ul>
+          </nav>
+        ))}
+      </div>
 
       <div className="tabContent">
-        {tabData.map((data, index) =>(
-            <p className= "tab-content" key= {index}>{tabData[tabs].content}</p>
+        {tabData.map((data, index) => (
+          <p className="tab-content" key={index}>
+            {tabData[tabs].content}
+          </p>
         ))}
-        
       </div>
     </div>
   );
