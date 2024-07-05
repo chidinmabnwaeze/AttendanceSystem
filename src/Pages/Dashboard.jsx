c07e0a5f117f7c372080135698c0158a7dd29575
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-05 18:46:17 +0100

    dash

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
+import IntroBar from '../components/IntroBar'
+import usersIcon from '../assets/icons/fa_users.svg'
 
 const Dashboard = () => {
 const [count, setCount] = useState([
     {
         title:"No of Total staff",
-        img : ,
+        img : usersIcon,
         staffCount: 100
     }
 ])
@@ -15,16 +17,17 @@ const [count, setCount] = useState([
     <div className='hero'>
       <Header/> 
       <Sidebar/>
+      <IntroBar/>
 
       <div className="card-count">
         <div className="card">
             {count.map((countCard, index)=>{
-                <div className="box">
+                <div className="box" key={index}>
                   <div className="box-title">
                     <p>{countCard.title}</p>
                     </div>  
                     <div className="box-number">
-                        <img src={} alt="" />
+                        <img src={countCard.img} alt="" />
                         <p className='number'>{countCard.staffCount}</p>
                     </div>
                 </div>
---
+++ src/components/IntroBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IntroBar = () => {
+  return (
+    <div className='introBar'>
+       <img className='anime' src="" alt="" /> 
+       <p>Welcome Sarah</p>
+    </div>
+  )
+}
+
+export default IntroBar
