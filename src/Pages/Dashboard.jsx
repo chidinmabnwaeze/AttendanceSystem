92112e2b43090f39c6413c6e7ec4bec998553215
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-07-05 03:53:02 +0100

    dash

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 
 const Dashboard = () => {
+const [count, setCount] = useState([
+    {
+        
+    }
+])
+
   return (
     <div className='hero'>
       <Header/> 
-      <Sidebar/> 
+      <Sidebar/>
+
+      <div className="card-count">
+        <div className="card">
+            {}
+        </div>
+        </div> 
     </div>
   )
 }
