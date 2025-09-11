688d485062700a9b9f7f27a3265de0ef4d4eb817
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2025-09-11 03:12:07 +0100

    collapseable menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Menu } from "lucide-react";
 
 const Header = () => {
   return (
-       <header className="attendance">
-        <div className="header-text">
-            <h5>ATTENDANCE CLOCKING SYSTEM</h5>
-        </div>
+    <header className="attendance flex justify-between">
+      {/* collapsable menu */}
+      <div className="menu">
+        <Menu />
+      </div>
+      <div className="header-text">
+        <h5>ATTENDANCE CLOCKING SYSTEM</h5>
+      </div>
     </header>
-   
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
